820e2b87a0a379a4fe2520404fea808c648d76f8
kuciapakrystian <kuciapakrystian@outlook.com>
2023-12-21 19:44:35 +0100

    version

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+// Write a script to manage the login form.
+// 1. Form submission (form.login-form) must be processed on the submit event.
+// 2. The page must not reload when the form is submitted.
+// 3. If the form has empty fields, display alert saying that all fields must be filled in.
+// 4. As soon as the user has filled in all the fields and submitted the form, collect the field values into an object, where the field name will be the property name and the field value will be the property value. Use the elements property to access form elements.
+// 5. Display the object with the entered data in the console and clear the values of the form fields using the reset method.
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  this.reset();
+  console.log(formData);
+}
