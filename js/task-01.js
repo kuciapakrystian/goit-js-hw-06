882b91539ff1681761a7ba2bd21910df96038545
kuciapakrystian <kuciapakrystian@outlook.com>
2024-01-23 20:54:37 +0100

    final version

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,7 @@
 const numOfCategEl = document.querySelector("#categories");
-
 const itemEl = numOfCategEl.childElementCount;
-
 console.log(`Number of categories: ${itemEl}`);
 const totalOfEl = document.querySelectorAll(".item");
-
 const categoryOfEl = totalOfEl.forEach((e) =>
   console.log(
     `Category: ${e.firstElementChild.textContent} Elemenets: ${e.lastElementChild.children.length}`
